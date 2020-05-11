1f3a6270b9874cf73a5e600029819a76661ce013
Nancy4510 <nancy4510@gmail.com>
2020-05-10 23:56:07 -0400

    Added a new file called api.js

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const config = require('../config')
+const store = require('../store')
+
+const signUp = function (formData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data: formData
+  })
+}
+
+const signIn = function (formData) {
+  return
+}
