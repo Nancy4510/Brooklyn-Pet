e29cb57474a9620308d748645fad08a8b0989160
Nancy4510 <nancy4510@gmail.com>
2020-06-13 21:09:47 -0400

    Changes made to a few files
     I made a few changes to a few files like the app.js
     file where the sign out button is hidden from the users
     in the home page and once a user signs in or signs up, they
     can see the button. I added a bit of more styling to the site as well.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -8,4 +8,58 @@ const successMessage = function (newText) {
   $('#message').addClass('success')
 }
 
-const failureMessage = function (newText)
+const failureMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+}
+
+const onSignUpSucess = function () {
+  successMessage('You have successfully signed up! Noe you can sign in!')
+  $('form').trigger('reset')
+}
+
+const onSignUpFailure = function () {
+  failureMessage('Sorry, signed up failed. Please try again.')
+}
+
+const onSignInSuccess = function (responseData) {
+  successMessage('Signed in successfully!')
+  store.user = responseData.user
+  $('.signIn').hide()
+  $('form').trigger('reset')
+}
+
+const onSignInFailure = function () {
+  failureMessage('Please sign up first or try again')
+}
+
+const onChangePasswordSuccess = function () {
+  successMessage('Changed password successfully!')
+  $('.changePassword').hide()
+  $('form').trigger('reset')
+}
+
+const onChangePasswordFailure = function () {
+  failureMessage('Change password failed')
+}
+
+const onSignOutSuccess = function () {
+  successMessage('Signed out successfully!')
+  $('form').trigger('reset')
+}
+
+const onSignOutFailure = function () {
+  failureMessage('Sorry, signed out failed')
+}
+
+module.exports = {
+  onSignUpSucess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
