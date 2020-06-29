845a1b69a605288ed34172a9f8e2a004dc4a9925
Nancy4510 <nancy4510@gmail.com>
2020-06-29 00:36:06 -0400

    Added reservation form
    I added a reservation form for clients to make for their pet.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -14,7 +14,7 @@ $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
-  $('#sign-out').on('submit', authEvents.onSignOut)
+  $('#sign-out').on('click', authEvents.onSignOut)
   $('.changePassword').hide()
   $('.signOut').hide()
 
